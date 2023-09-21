4c82b5a2bcd5e20e5cbec920372787158d43f413
Evan <evaan@users.noreply.github.com>
2023-09-20 22:17:34 -0230

    Rewrote interface

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,19 @@
 
 AsyncWebServer server(80);
 String logs = "";
-int unixTime = 0;
+int status = 0;
 
 String getTime() {
-  time_t time = (time_t)(unixTime + millis())/1000;
+  time_t time = (time_t)(millis())/1000;
   char buffer[10];
-  strftime(buffer, 10, "[%H:%M:%S]", gmtime(&time));
+  strftime(buffer, 10, "[%H:%M:%S]", localtime(&time));
   return buffer;
 }
 
-void start(void* parameter) {
-  AsyncWebServerRequest *request = (AsyncWebServerRequest*)parameter;
-  if (unixTime = 0) {request->send_P(420, "text/plain", "Refresh needed."); return;}
-  request->send_P(200, "text/plain", "Success!");
-  logs+=getTime() + " Starting profile...";
-  //do the profile thing
-  sleep(10); //idk change this
-  logs+=getTime() + " Resurfacing profiler...";
-  //do the profile thing that make it go back up
-  sleep(10); //change it once again use a sensor or somethin
-  logs+=getTime() + " Profile complete!";
+void start(AsyncWebServerRequest *request) {
+  if (status == 0) {
+    logs += "Profiler started at " + getTime() + "\n";
+  }
 }
 
 void setup() {
@@ -33,9 +26,25 @@ void setup() {
   WiFi.softAPConfig(IPAddress(192, 168, 0, 1), IPAddress(192, 168, 0, 1), IPAddress(255, 255, 255, 0));
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {request->send(LittleFS, "/index.html", "text/html");});
   server.on("/jquery.min.js", HTTP_GET, [](AsyncWebServerRequest *request) {request->send(LittleFS, "/jquery.min.js");});
-  server.on("/start", HTTP_GET, [](AsyncWebServerRequest *request) {xTaskCreatePinnedToCore(start, "start", 1024, request, 0, NULL, 0);});
-  server.on("/time", HTTP_GET, [](AsyncWebServerRequest *request) {unixTime = request->getParam("time")->value().toInt() - millis(); request->send_P(200, "text/plain", "Time set!");});
+  server.on("/dygraph.min.js", HTTP_GET, [](AsyncWebServerRequest *request) {request->send(LittleFS, "/dygraph.min.js");});
+  server.on("/start", HTTP_GET, [](AsyncWebServerRequest *request) {start(request);});
   server.on("/logs", HTTP_GET, [](AsyncWebServerRequest *request) {request->send_P(200, "text/plain", logs.c_str());});
 }
 
-void loop() {}
+void loop() {
+  //status 0: idle
+  //status 1: going down
+  //status 2 going up
+  switch(status) {
+    case 1:
+      //if pressure/depth is at a certain point then switch to status 2 and change servo/whatever controls buoyancy engine
+      if (millis() % 5000) logs += "EX99 - " + getTime() + " - XX.X KPA - XX.X M\n";
+      break;
+    case 2:
+      //same as first case
+      if (millis() % 5000) logs += "EX99 - " + getTime() + " - XX.X KPA - XX.X M\n";
+      break;
+    default:
+      return;
+  }
+}
